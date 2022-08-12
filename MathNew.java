dcf209e95ee87820764258ccf427301042da65fb
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-12 18:37:47 +0530

    Add files via upload

---
+++ MathNew.java
@@ -0,0 +1,23 @@
+//Write a Java program to print the sum (addition), multiply, subtract, divide and
+//remainder of two numbers.
+//Test Data:
+//Input first number: 125
+//Input second number: 24
+class MathNew{
+public static void main(String[]args){
+System.out.println("the two numbers is:");
+int a=125;
+int b=24;
+int sum=a+b;
+int multi=a*b;
+int sub=a-b;
+int divi=a/b;
+int remain=a%b;
+System.out.println(a+" "+b);
+System.out.println("Sum of two numbers:"+sum);
+System.out.println("Multi of two numbers:"+multi);
+System.out.println("Sub of two numbers:"+sub);
+System.out.println("Divi of two numbers:"+divi);
+System.out.println("Remain of two numbers:"+remain);
+}
+}
