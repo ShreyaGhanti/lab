0d971fc5ae6e7340466f8a9aff3f06f4feb1b6c1
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-18 21:47:42 +0530

    Add files via upload

---
+++ 12 august java lab/Swapping.java
@@ -0,0 +1,17 @@
+//Swapping two variables refers to mutually exchanging the values of thevariables. Generally, this is done with the data in memory
+
+public class Swapping{ 
+public static void main(String[]args){
+int a=10;
+int b=20;
+System.out.println("The one variable a is="+a );
+System.out.println("The second variable b is="+b );
+
+int temp;
+temp=a;
+a=b;
+b=temp;
+System.out.println("The one number after swapping a ="+a );
+System.out.println("The second number after swapping b ="+b );
+}
+}
