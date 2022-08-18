90889c83245eb2834f1ee073775a422aa1293756
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-18 19:27:29 +0530

    Add files via upload

---
+++ New8.java
@@ -0,0 +1,24 @@
+// Write a Java program that accepts two integers from the user and then
+//prints the sum, the difference, the product, the average, the distance (the
+//difference between integer), the maximum (the larger of the two integers), the
+//minimum (smaller of the two integers).
+public class New8{
+public static void main(String[]args){
+System.out.println("the two numbers is:");
+int a=40;
+int b=20;
+int sum=a+b;
+int multi=a*b;
+int sub=a-b;
+int divi=a/b;
+int ave=a+b/2;
+System.out.println(a+""+b);
+System.out.println("Sum of two numbers:"+sum);
+System.out.println("Multi of two numbers:"+multi);
+System.out.println("Sub of two numbers:"+sub);
+System.out.println("Divi of two numbers:"+divi);
+System.out.println("Ave of the two numbers;"+ave);
+
+}
+}
+
