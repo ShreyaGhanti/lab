727b924fe4e9cd4cecb19ae43b13e893e00a9522
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-09-08 19:16:12 +0530

    Add files via upload

---
+++ Newjava.java
@@ -0,0 +1,14 @@
+//Q.3... Develop a public java class and make sure nobody can create
+//any object of this class from outside the class
+
+class java{
+	private java(){
+}}
+public class Newjava {
+
+	public static void main(String[] args) {
+		Newjava obj=new Newjava();
+
+	}
+
+}
