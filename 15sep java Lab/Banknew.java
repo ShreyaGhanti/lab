4f9d4a903df7bafc0d13db8ba5bb20408fecef52
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-09-15 17:39:51 +0530

    Add files via upload

---
+++ 15sep java Lab/Banknew.java
@@ -0,0 +1,25 @@
+//Consider a scenario where Bank is a class that provides functionality to get the rate
+//of interest. However, the rate of interest varies according to banks. For example, SBI,
+//ICICI and AXIS banks could provide 8%, 7%, and 9% rate of interest.
+class Bank{ 
+int getRateOfInterest(){return 0;} 
+} 
+ 
+class SBI extends Bank{ 
+int getRateOfInterest(){return 8;} 
+} 
+ 
+class ICICI extends Bank{ 
+int getRateOfInterest(){return 7;} 
+} 
+class AXIS extends Bank{ 
+int getRateOfInterest(){return 9;}}
+class Banknew{ 
+public static void main(String args[]){ 
+SBI s=new SBI(); 
+ICICI i=new ICICI(); 
+AXIS a=new AXIS(); 
+System.out.println("SBI Rate of Interest: "+s.getRateOfInterest()); 
+System.out.println("ICICI Rate of Interest: "+i.getRateOfInterest()); 
+System.out.println("AXIS Rate of Interest: "+a.getRateOfInterest()); 
+} } 
